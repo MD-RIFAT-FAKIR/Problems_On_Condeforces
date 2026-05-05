d157c43fcf0564c2e38afef5740e8df804ba0a69
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-05-05 08:07:50 +0600

    Find Maximum number form series(N numbers)

---
+++ loop/max.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int number;
+    scanf("%d", &number);
+    int max = number;
+
+    for (int i=1; i<n; i++) {
+        scanf("%d", &number);
+        if(number > max){
+            max = number;
+        }
+    }
+    printf("%d", max);
+
+    return 0;
+}
