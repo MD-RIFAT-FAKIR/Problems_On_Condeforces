1145bb0b96d304d364be4bd0a847177d1d5da36b
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-07-04 13:53:46 +0600

    Given a number N. Print a diamond that has 2N rows.

---
+++ loop/W_Shape_3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    //first half triangle
+    for(int i=1; i<=n; i++) {
+        //spaces before first triangle
+        for(int j=1; j<=n-i; j++) { 
+            printf(" ");
+        }
+        //star for first triangle
+        for(int k=1; k<=(i*2)-1; k++) {
+            printf("*");
+        }
+        //new line after every outerloop iteration 
+        printf("\n");
+    }
+    
+    //second half tringle
+    for(int l=0; l<n; l++) {
+        //spaces before second half triangle
+        for(int m=0; m<l; m++) {
+            printf(" ");
+        }
+        //star for second half triangle
+        for(int p=0; p<(2*(n-l)-1); p++) {
+            printf("*");
+        }
+        //new line after every outerloop iteration
+        printf("\n");
+    }
+}
